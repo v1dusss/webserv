d2a629c5c651e033a880b3937abd92795862e6bd
Emil Ebert <emilebert@icloud.com>
2025-05-02 00:34:30 +0200

    Implement POST request handling with file creation and appending logic

--- src/server/requestHandler/PostRequest.cpp
+++ src/server/requestHandler/PostRequest.cpp
@@ -1,5 +1,22 @@
 #include "RequestHandler.h"
 
+#include <sys/stat.h>
+#include <fstream>
+
 HttpResponse RequestHandler::handlePost() {
-return HttpResponse();
+    struct stat fileStat;
+    bool fileExists = (stat(routePath.c_str(), &fileStat) == 0 && !S_ISDIR(fileStat.st_mode));
+
+    std::ofstream file(routePath, std::ios::app | std::ios::binary);
+    if (!file)
+        return Response::customResponse(HttpResponse::StatusCode::INTERNAL_SERVER_ERROR,
+                                        "500 Internal Server Error");
+
+    file << request.body;
+    file.close();
+
+    HttpResponse response(fileExists ? HttpResponse::StatusCode::OK : HttpResponse::StatusCode::CREATED);
+    response.setBody(fileExists ? "200 OK: Data appended" : "201 Created: File created");
+    response.setHeader("Content-Type", "text/plain");
+    return response;
 }
