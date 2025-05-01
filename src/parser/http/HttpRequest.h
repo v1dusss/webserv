3da9988143bbec4a4164d62bffd8c57b13b0c5a8
Emil Ebert <emilebert@icloud.com>
2025-05-01 19:57:55 +0200

    add HttpRequest class for handling HTTP requests and headers

---
+++ src/parser/http/HttpRequest.h
@@ -0,0 +1,41 @@
+//
+// Created by Emil Ebert on 01.05.25.
+//
+
+#ifndef HTTPREQUEST_H
+#define HTTPREQUEST_H
+
+#include <string>
+#include <unordered_map>
+#include "config/config.h"
+#include <iostream>
+
+class HttpRequest {
+public:
+    HttpMethod method;
+    std::string uri;
+    std::string version;
+    std::unordered_map<std::string, std::string> headers;
+    std::string body;
+
+    HttpRequest() : method(GET) {
+    }
+
+    std::string getHeader(const std::string &name) const {
+        auto it = headers.find(name);
+        return it != headers.end() ? it->second : "";
+    }
+
+    void printRequest() const {
+        std::cout << "Method: " << method << "\n"
+                << "URI: " << uri << "\n"
+                << "Version: " << version << "\n"
+                << "Headers:\n";
+        for (const auto &header: headers) {
+            std::cout << header.first << ": " << header.second << "\n";
+        }
+        std::cout << "Body: " << body;
+    }
+};
+
+#endif //HTTPREQUEST_H
