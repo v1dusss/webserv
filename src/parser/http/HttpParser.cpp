6637448dfb9f2778952ea556c5787498fc45682a
Emil Ebert <emilebert@icloud.com>
2025-05-03 16:51:03 +0200

    enhance HttpParser with request line length validation and header format checks and

--- src/parser/http/HttpParser.cpp
+++ src/parser/http/HttpParser.cpp
@@ -8,6 +8,7 @@
 #include "common/Logger.h"
 #include <sstream>
 #include <algorithm>
+#include <regex>
 
 HttpParser::HttpParser()
     : state(ParseState::REQUEST_LINE),
@@ -46,13 +47,20 @@ bool HttpParser::parse(const char *data, size_t length) {
         }
     }
 
-    return state == ParseState::COMPLETE;
+    return false;
 }
 
 bool HttpParser::parseRequestLine() {
     size_t endPos = buffer.find("\r\n");
     if (endPos == std::string::npos)
         return false;
+
+    constexpr size_t MAX_REQUEST_LINE_LENGTH = 8192;
+    if (endPos > MAX_REQUEST_LINE_LENGTH) {
+        Logger::log(LogLevel::ERROR, "Request line too long");
+        state = ParseState::ERROR;
+        return false;
+    }
 
     std::string line = buffer.substr(0, endPos);
     buffer.erase(0, endPos + 2);
@@ -61,12 +69,33 @@ bool HttpParser::parseRequestLine() {
     std::string methodStr, uri, version;
 
     if (!(iss >> methodStr >> uri >> version)) {
-        Logger::log(LogLevel::ERROR, "Invalid HTTP request line");
+        Logger::log(LogLevel::ERROR, "Invalid HTTP request line format");
         state = ParseState::ERROR;
         return false;
     }
 
-    request->method = stringToMethod(methodStr);
+    auto method = stringToMethod(methodStr);
+    if (method == std::nullopt) {
+        Logger::log(LogLevel::ERROR, "Invalid HTTP method: " + methodStr);
+        state = ParseState::ERROR;
+        return false;
+    }
+
+    if ((uri.empty() || uri[0] != '/') && uri.find("://") == std::string::npos) {
+        Logger::log(LogLevel::ERROR, "Invalid URI format: " + uri);
+        state = ParseState::ERROR;
+        return false;
+    }
+
+    if (version.length() < 8 || version.substr(0, 5) != "HTTP/" ||
+        version[5] != '1' || version[6] != '.' ||
+        (version[7] != '0' && version[7] != '1')) {
+        Logger::log(LogLevel::ERROR, "Invalid HTTP version: " + version);
+        state = ParseState::ERROR;
+        return false;
+    }
+
+    request->method = method.value();
     request->uri = uri;
     request->version = version;
 
@@ -75,6 +104,9 @@ bool HttpParser::parseRequestLine() {
 }
 
 bool HttpParser::parseHeaders() {
+    constexpr size_t MAX_HEADERS = 100;
+    size_t headerCount = 0;
+
     while (true) {
         size_t endPos = buffer.find("\r\n");
         if (endPos == std::string::npos)
@@ -104,15 +136,23 @@ bool HttpParser::parseHeaders() {
             return true;
         }
 
-        std::string line = buffer.substr(0, endPos);
-        buffer.erase(0, endPos + 2);
-
-        size_t colonPos = line.find(':');
-        if (colonPos == std::string::npos) {
+        if (++headerCount > MAX_HEADERS) {
+            Logger::log(LogLevel::ERROR, "Too many headers in request");
             state = ParseState::ERROR;
             return false;
         }
 
+        std::string line = buffer.substr(0, endPos);
+        buffer.erase(0, endPos + 2);
+
+        static const std::regex headerRegex(R"(^[!#$%&'*+\-.^_`|~0-9A-Za-z]+:[ \t]*[^\r\n]*$)");
+        if (!std::regex_match(line, headerRegex)) {
+            Logger::log(LogLevel::ERROR, "Invalid header format: " + line);
+            state = ParseState::ERROR;
+            return false;
+        }
+
+        size_t colonPos = line.find(':');
         std::string name = line.substr(0, colonPos);
         std::string value = line.substr(colonPos + 1);
 
@@ -124,11 +164,45 @@ bool HttpParser::parseHeaders() {
 
 bool HttpParser::parseBody() {
     if (chunkedTransfer) {
-        Logger::log(LogLevel::ERROR, "Chunked encoding not yet implemented");
-        state = ParseState::ERROR;
-        return false;
-    }
+        std::string fullBody;
 
+        while (true) {
+
+            size_t sizeEndPos = buffer.find("\r\n");
+            if (sizeEndPos == std::string::npos)
+                return false; // Need more data
+
+            std::string sizeHex = buffer.substr(0, sizeEndPos);
+
+            size_t semicolonPos = sizeHex.find(';');
+            if (semicolonPos != std::string::npos) {
+                sizeHex = sizeHex.substr(0, semicolonPos);
+            }
+
+            unsigned long chunkSize;
+            try {
+                chunkSize = std::stoul(sizeHex, nullptr, 16);
+            } catch (...) {
+                Logger::log(LogLevel::ERROR, "Invalid chunk size format");
+                state = ParseState::ERROR;
+                return false;
+            }
+
+            buffer.erase(0, sizeEndPos + 2);
+
+            if (chunkSize == 0) {
+                state = ParseState::COMPLETE;
+                return true;
+            }
+
+            if (buffer.length() < chunkSize + 2)
+                return false;
+
+            fullBody.append(buffer, 0, chunkSize);
+
+            buffer.erase(0, chunkSize + 2);
+        }
+    }
     if (buffer.length() < contentLength)
         return false;
 
@@ -138,15 +212,15 @@ bool HttpParser::parseBody() {
     return true;
 }
 
-HttpMethod HttpParser::stringToMethod(const std::string &method) {
-    if (method == "GET") return GET;
-    if (method == "POST") return POST;
-    if (method == "PUT") return PUT;
-    if (method == "DELETE") return DELETE;
-    if (method == "PATCH") return PATCH;
-    if (method == "OPTIONS") return OPTIONS;
+std::optional<HttpMethod> HttpParser::stringToMethod(const std::string &method) {
+    if (method == "GET") return std::make_optional(GET);
+    if (method == "POST") return std::make_optional(POST);
+    if (method == "PUT") return std::make_optional(PUT);
+    if (method == "DELETE") return std::make_optional(DELETE);
+    if (method == "PATCH") return std::make_optional(PATCH);
+    if (method == "OPTIONS") return std::make_optional(OPTIONS);
 
-    return GET;
+    return std::nullopt;
 }
 
 std::shared_ptr<HttpRequest> HttpParser::getRequest() const {
