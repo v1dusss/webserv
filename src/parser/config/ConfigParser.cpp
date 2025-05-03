203671e99875255cdfd6b14b3407e10cab0c7189
Emil Ebert <emilebert@icloud.com>
2025-05-03 16:47:14 +0200

    implement first functions of ConfigParser

--- src/parser/config/ConfigParser.cpp
+++ src/parser/config/ConfigParser.cpp
@@ -6,43 +6,87 @@
 #include <regex>
 #include <parser/http/HttpParser.h>
 
-std::vector<std::string> ConfigBlock::getDirective(const std::string& key) const {
-    auto it = directives.find(key);
-    return it != directives.end() ? it->second : std::vector<std::string>{};
+ConfigParser::ConfigParser() : rootBlock{"root", {}, {}}, currentLine(0), currentFilename(""), parseSuccessful(true) {
+    serverDirectives = {
+        "listen", "server_name", "root", "index", "client_max_body_size",
+        "timeout", "client_header_buffer_size", "keepalive_timeout",
+        "keepalive_requests", "error_page"
+    };
+
+    locationDirectives = {
+        "root", "index", "autoindex", "client_max_body_size", "alias",
+        "deny", "allowed_methods", "error_page", "return"
+    };
+
+    validDirectivePrefixes = {"cgi"};
 }
 
-std::string ConfigBlock::getStringValue(const std::string& key, const std::string& defaultValue) const {
-    auto values = getDirective(key);
-    return values.empty() ? defaultValue : values[0];
-}
-
-int ConfigBlock::getIntValue(const std::string& key, int defaultValue) const {
-    auto values = getDirective(key);
-    if (values.empty()) return defaultValue;
-    try {
-        return std::stoi(values[0]);
-    } catch (...) {
-        return defaultValue;
+bool ConfigParser::parse(const std::string& filename) {
+    std::ifstream file(filename);
+    if (!file.is_open()) {
+        std::cerr << RED << "Failed to open config file: " << filename << RESET << std::endl;
+        return false;
     }
+
+    currentFilename = filename;
+    currentLine = 0;
+    parseSuccessful = true;
+
+    bool result = parseBlock(file, rootBlock);
+    return result && parseSuccessful;
 }
 
-std::map<std::string, std::vector<std::string>> ConfigBlock::findDirectivesByPrefix(const std::string& prefix) const {
-    std::map<std::string, std::vector<std::string>> result;
-    for (const auto& [key, value] : directives) {
-        if (key.substr(0, prefix.length()) == prefix) {
-            result[key] = value;
+bool ConfigParser::isValidDirective(const std::string& directive, const std::string& blockType) const {
+    if (blockType == "server" &&
+        std::find(serverDirectives.begin(), serverDirectives.end(), directive) != serverDirectives.end()) {
+        return true;
+    }
+
+    if (blockType == "location" &&
+        std::find(locationDirectives.begin(), locationDirectives.end(), directive) != locationDirectives.end()) {
+        return true;
+    }
+
+    for (const auto& prefix : validDirectivePrefixes) {
+        if (directive.substr(0, prefix.length()) == prefix) {
+            return true;
         }
     }
-    return result;
+
+    return false;
 }
 
-std::vector<ConfigBlock*> ConfigBlock::findBlocks(const std::string& blockName) {
-    std::vector<ConfigBlock*> result;
-    for (auto& child : children) {
-        if (child.name == blockName) {
-            result.push_back(&child);
+bool ConfigParser::validateDigitsOnly(const std::string& value, const std::string& directive) {
+    std::regex digitsOnly("^\\d+$");
+    if (!std::regex_match(value, digitsOnly)) {
+        reportError("Invalid value for " + directive + ": '" + value + "' - should contain only digits");
+        parseSuccessful = false;
+        return false;
+    }
+    return true;
+}
+
+bool ConfigParser::validateListenValue(const std::string& value) {
+    size_t colonPos = value.find(':');
+
+    if (colonPos == std::string::npos) {
+        return validateDigitsOnly(value, "listen");
+    }
+
+    std::string port = value.substr(colonPos + 1);
+    return validateDigitsOnly(port, "listen");
+}
+
+std::vector<ServerConfig> ConfigParser::getServerConfigs() const {
+    std::vector<ServerConfig> servers;
+
+    for (const auto& child : rootBlock.children) {
+        if (child.name == "server") {
+            servers.push_back(parseServerBlock(child));
         }
     }
-    return result;
+
+    return servers;
 }
 
+}
