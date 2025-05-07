0d9ac971dee1eb3f2716a8be66801b1ce64f7585
Emil Ebert <emilebert@icloud.com>
2025-05-07 13:23:47 +0200

    add timestamp to log messages for improved context

--- src/common/Logger.cpp
+++ src/common/Logger.cpp
@@ -8,22 +8,29 @@ LogLevel Logger::currentLogLevel = LogLevel::DEBUG;
 
 void Logger::log(const LogLevel level, const std::string &message) {
     if (level <= currentLogLevel) {
+        const auto now = std::chrono::system_clock::now();
+        const auto time_t = std::chrono::system_clock::to_time_t(now);
+        std::tm* timeInfo = std::localtime(&time_t);
+
+        char timeBuffer[20];
+        std::strftime(timeBuffer, sizeof(timeBuffer), "%Y-%m-%d %H:%M:%S", timeInfo);
+
         std::string prefix;
         switch (level) {
             case LogLevel::INFO: prefix = GREEN "[INFO] ";
-                break;
+            break;
             case LogLevel::WARNING: prefix = YELLOW "[WARNING] ";
-                break;
+            break;
             case LogLevel::ERROR: prefix = RED "[ERROR] ";
-                break;
+            break;
             case LogLevel::DEBUG: prefix = CYAN "[DEBUG] ";
-                break;
+            break;
             default:
                 break;
         }
         if (level == LogLevel::ERROR)
-            std::cerr << prefix << message << std::endl;
+            std::cerr << BLUE "[" << timeBuffer << "]" << RESET << " " << prefix << message << RESET << std::endl;
         else
-            std::cout << prefix << message << RESET << std::endl;
+            std::cout << BLUE "[" << timeBuffer << "]" << RESET << " " << prefix << message << RESET << std::endl;
     }
 }
