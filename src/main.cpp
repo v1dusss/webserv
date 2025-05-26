d6c2425e74eb5f028e3994eeaebcb2ca980b7047
Emil Ebert <emilebert@icloud.com>
2025-05-26 17:27:54 +0200

    add temporary directory creation

--- src/main.cpp
+++ src/main.cpp
@@ -5,18 +5,22 @@
 #include <iostream>
 #include "server/ServerPool.h"
 #include <csignal>
-
+#include <webserv.h>
+#include <sys/stat.h>
+#include <cerrno>
+#include <cstring>
+#include <server/FdHandler.h>
+#include <unistd.h>
 #include "common/Logger.h"
 
 ServerPool serverPool;
 
-void signalHandler(const int signum) {
-    std::cout << std::endl;
+static void signalHandler(const int signum) {
     serverPool.stop();
     (void) signum;
 }
 
-void setupSignalHandler() {
+static void setupSignalHandler() {
     struct sigaction sa{};
     sa.sa_handler = signalHandler;
     sa.sa_flags = SA_RESTART;
@@ -28,12 +32,26 @@ void setupSignalHandler() {
     }
 }
 
+static void createTempDir() {
+    if (mkdir(TEMP_DIR_NAME, 0777) == -1 && errno != EEXIST) {
+        Logger::log(LogLevel::ERROR, "Failed to create temporary directory: " + std::string(strerror(errno)));
+        exit(1);
+    }
+}
+
 int main(const int argc, char **argv) {
     if (argc < 2) {
         Logger::log(LogLevel::ERROR, "Not enough arguments");
         std::cerr << "Usage: " << argv[0] << " <config_file>" << std::endl;
         return 1;
     }
+
+    //::signal(SIGPIPE, SIG_IGN);
+
+
+    std::cout << "start pid: " << getpid() << std::endl;
+
+    createTempDir();
     if (!serverPool.loadConfig(argv[1]))
         return 1;
 
